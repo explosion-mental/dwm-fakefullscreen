33d91607ea116f7161a62dbc00a5b128c2c9e0ef
explosion-mental <explosion0mental@gmail.com>
2022-05-03 12:00:28 -0500

    add Firefox to start as fakefullscreen

--- config.def.h
+++ config.def.h
@@ -28,7 +28,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   isfakefullscreen  monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           0,                -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,                -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           1,                -1 },
 };
 
 /* layout(s) */
