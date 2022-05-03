f1516ea825fca9dac42154dd0e6be6aa9d7c64d5
explosion-mental <explosion0mental@gmail.com>
2022-05-03 12:06:26 -0500

    on my end the class of Firefox is `firefox` (no capital)

--- config.h
+++ config.h
@@ -28,7 +28,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   isfakefullscreen  monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           0,                -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           1,                -1 },
+	//{ "Firefox",  NULL,       NULL,       1 << 8,       0,           1,                -1 },
+	{ "firefox",  NULL,       NULL,       1 << 8,       0,           1,                -1 },
 };
 
 /* layout(s) */
